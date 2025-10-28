fd6d46e14fa7fc3bd7e473ea9f725c2f8868b203
Sergey Korolyov <s.cruze99@yandex.ru>
2025-10-28 22:29:28 +0300

    Правка деплоя

--- app/mailers/previews/user_request_mailer_preview.rb
+++ app/mailers/previews/user_request_mailer_preview.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-class UserRequestMailerPreview < ActionMailer::Preview
-  def confirmation
-    ur = UserRequest.last || UserRequest.new(id: 1, name: "Alex", email: "alex@example.com", created_at: Time.current)
-    UserRequestMailer.confirmation(ur.id)
-  end
+module Previews
+  class UserRequestMailerPreview < ActionMailer::Preview
+    def confirmation
+      ur = UserRequest.last || UserRequest.new(id: 1, name: "Alex", email: "alex@example.com", created_at: Time.current)
+      UserRequestMailer.confirmation(ur.id)
+    end
 
-  def team_digest
-    ur = UserRequest.last || UserRequest.new(id: 1, name: "Alex", email: "alex@example.com", created_at: Time.current)
-    UserRequestMailer.team_digest(ur.id, recipients: %w[founder@example.com team@example.com])
+    def team_digest
+      ur = UserRequest.last || UserRequest.new(id: 1, name: "Alex", email: "alex@example.com", created_at: Time.current)
+      UserRequestMailer.team_digest(ur.id, recipients: %w[founder@example.com team@example.com])
+    end
   end
 end
